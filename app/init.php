4861a9b427a24ed5bc8d9dfee68b71ba4cd9fb2d
Xiao Jia <stfairy@gmail.com>
2012-08-05 16:15:06 +0800

    detect more about APC availability

--- app/init.php
+++ app/init.php
@@ -2,7 +2,7 @@
 fSession::setPath(SESSIONS_PATH);
 fSession::setLength('1 day 2 hours');
 fORMDatabase::attach(new fDatabase('mysql', DB_NAME, DB_USER, DB_PASS, DB_HOST));
-if (APC_ENABLED && ENABLE_SCHEMA_CACHING) {
+if (function_exists('apc_cache_info') && APC_ENABLED && ENABLE_SCHEMA_CACHING) {
   fORM::enableSchemaCaching(new fCache('apc'));
 }
 fORMDatabase::retrieve()->registerHookCallback('run', 'profiler_log_sql');
