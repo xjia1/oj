7fc44a41835a0e31105130663690f24883078047
Xiao Jia <stfairy@gmail.com>
2012-08-08 07:14:52 +0800

    add real name in ranklist

--- app/views/user/ranklist.php
+++ app/views/user/ranklist.php
@@ -20,7 +20,7 @@ include(__DIR__ . '/../layout/header.php');
     <?php foreach ($this->user_stats as $i => $user_stat): ?>
       <tr>
         <td><?php echo $i + 1 + ($this->page - 1) * $this->user_stats->getLimit(); ?></td>
-        <td><?php echo $user_stat->getUsername(); ?></td>
+        <td><?php echo $user_stat->getUsername(); ?> <?php echo Profile::fetchRealName($user_stat->getUsername()); ?></td>
         <td><?php echo $user_stat->getSolved(); ?></td>
         <td><?php echo $user_stat->getTried(); ?></td>
         <td><?php echo $user_stat->getSubmissions(); ?></td>
