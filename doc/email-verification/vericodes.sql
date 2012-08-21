32b41e472c5606b43674d5443dead7ae4efbfa73
Xiao Jia <stfairy@gmail.com>
2012-08-21 15:07:15 +0800

    add create table sqls for email verification

---
+++ doc/email-verification/vericodes.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2012 年 08 月 21 日 15:06
+-- 服务器版本: 5.5.24
+-- PHP 版本: 5.3.10-1ubuntu3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `online_judge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `vericodes`
+--
+
+CREATE TABLE IF NOT EXISTS `vericodes` (
+  `username` varchar(100) NOT NULL,
+  `vericode` varchar(200) NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `vericode` (`vericode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
