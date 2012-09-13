5943f265c09bcbdd4e8b1a792bcc2af09f55a9bc
Xiao Jia <stfairy@gmail.com>
2012-09-13 20:26:15 +0800

    add num of problems

--- app/views/report/index.php
+++ app/views/report/index.php
@@ -13,6 +13,7 @@ include(__DIR__ . '/../layout/header.php');
         <th>公开</th>
       <?php endif; ?>
       <th>标题</th>
+      <th>题目数目</th>
       <th>开始时间</th>
       <th>结束时间</th>
       <th>比赛时长</th>
@@ -31,6 +32,7 @@ include(__DIR__ . '/../layout/header.php');
           </td>
         <?php endif; ?>
         <td><a href="<?php echo SITE_BASE; ?>/contest/<?php echo $r->getId(); ?>"><?php echo fHTML::prepare($r->getTitle()); ?></a></td>
+        <td><?php echo count($r->getProblems()); ?></td>
         <td><?php echo $r->getStartDatetime(); ?></td>
         <td><?php echo $r->getEndDatetime(); ?></td>
         <td><?php echo $r->getDuration(); ?></td>
