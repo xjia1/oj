661229438bdb78f7b101fb1927a1b67ce047fea9
Xiao Jia <stfairy@gmail.com>
2012-09-13 20:41:02 +0800

    translate record show page

--- app/views/record/show.php
+++ app/views/record/show.php
@@ -1,42 +1,40 @@
 <?php
-$title = 'Record #' . $this->record->getId();
+$title = '#' . $this->record->getId();
 $stylesheets = array('verdicts');
 include(__DIR__ . '/../layout/header.php');
 ?>
 <div class="page-header">
   <h1>
-    Record #<?php echo $this->record->getId(); ?>
+    <?php echo $title; ?>
     <small class="record<?php echo str_replace(' ', '', $this->record->getResult()); ?>">
-      <?php echo $this->record->getResult(); ?>
+      <?php echo $this->record->getTranslatedResult(); ?>
     </small>
     <small>
-      / Score: <?php echo $this->record->getScore(); ?>
-      / Time: <?php echo $this->record->getTimeCost(); ?>
-      / Memory: <?php echo $this->record->getMemoryCost(); ?>
+      / 分数：<?php echo $this->record->getScore(); ?>
+      / 时间：<?php echo $this->record->getTimeCost(); ?>
+      / 内存：<?php echo $this->record->getMemoryCost(); ?>
     </small>
   </h1>
 </div>
 <div class="row">
   <div class="span9">
-    <h2>Judge Message</h2>
     <pre><?php echo fHTML::encode($this->record->getJudgeMessage()); ?></pre>
-    <h2>Code</h2>
     <pre class="prettyprint linenums"><?php echo fHTML::encode($this->record->getSubmitCode()); ?></pre>
   </div>
   <ul class="span3">
     <li>
-      Owner:
+      提交者：
       <?php echo fHTML::encode($this->record->getOwner()); ?>
       <?php echo fHTML::encode(Profile::fetchRealName($this->record->getOwner())); ?>
     </li>
     <li>
-      Problem:
+      题目编号：
       <a href="<?php echo SITE_BASE; ?>/problem/<?php echo $this->record->getProblemId(); ?>">
         <?php echo $this->record->getProblemId(); ?></a>
       <a href="<?php echo SITE_BASE; ?>/submit?problem=<?php echo $this->record->getProblemId(); ?>" class="icon-repeat"></a>
     </li>
-    <li>Language: <?php echo fHTML::encode($this->record->getLanguageName()); ?></li>
-    <li>Submit Time: <?php echo $this->record->getSubmitDatetime(); ?></li>
+    <li>语言：<?php echo fHTML::encode($this->record->getLanguageName()); ?></li>
+    <li>提交时间：<?php echo $this->record->getSubmitDatetime(); ?></li>
   </ul>
 </div>
 <?php
