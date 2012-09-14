9dd363dc8c7c01acdbefae59bc54a15c92f31191
Xiao Jia <stfairy@gmail.com>
2012-09-14 20:43:34 +0800

    disable session for anonymous users

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -1,13 +1,27 @@
 <?php
 class ApplicationController
 {
-  protected function cache_control($type, $time)
+  private static function to_gmt($now = NULL)
   {
-    header("Cache-Control: {$type},max-age={$time}");
+    return gmdate('D, d M Y H:i:s', ($now === NULL) ? time() : $now);
+  }
+  
+  protected function cache_control($type, $seconds)
+  {
+    header_remove('Pragma');
+    header("Cache-Control: {$type},max-age={$seconds}");
+    header('Expires: ' . static::to_gmt(time() + $seconds) . ' GMT');
   }
   
   protected function render($name)
   {
+    // before output page
+    // if the visitor is anonymous
+    // do NOT send Set-Cookie to enable caching of Varnish
+    if (!fAuthorization::checkLoggedIn()) {
+      header_remove('Set-Cookie');
+    }
+    // then output page
     include(__DIR__ . '/../views/' . $name . '.php');
   }
   
