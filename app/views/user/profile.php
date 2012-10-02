2940fd96d75c61950e9ebd209ea5fd680b39125b
slipper <slipper@slipper.laptop>
2012-10-02 12:52:20 +0800

    profile

--- app/views/user/profile.php
+++ app/views/user/profile.php
@@ -6,48 +6,43 @@ include(__DIR__ . '/../layout/header.php');
   <h1><?php echo $title; ?></h1>
   <!--<a class="btn btn-primary"  href="./change/info">修改信息</a>-->
 </div>
-  <dl>
-    <dt><label class="control-lab"><h3>姓名：<?php echo Profile::fetchRealName(fAuthorization::getUserToken()); ?></h3></dt>
+  <ul>
+    <li><label class="control-lab"><h3>姓名：<?php echo Profile::fetchRealName(fAuthorization::getUserToken()); ?></h3></label></li>
 
-    <dt><label class="control-lab"><h3>班级：<?php echo Profile::fetchClassName(fAuthorization::getUserToken()); ?></h3></dt>
+    <li><label class="control-lab"><h3>班级：<?php echo Profile::fetchClassName(fAuthorization::getUserToken()); ?></h3></label></li>
 
-    <dt><label class="control-lab"><h3>手机：<?php echo Profile::fetchPhoneNumber(fAuthorization::getUserToken()); ?></h3></dt>
+    <li><label class="control-lab"><h3>手机：<?php echo Profile::fetchPhoneNumber(fAuthorization::getUserToken()); ?></h3></label></li>
 
-    <dt><label class="control-lab"><h3>邮箱：<?php echo UserEmail::fetch(fAuthorization::getUserToken()); ?></h3></dt>
+    <li><label class="control-lab"><h3>邮箱：<?php echo UserEmail::fetch(fAuthorization::getUserToken()); ?></h3></label></li>
 
-    <td><label class="control-lab"><h3>已解决问题：<?php $solved = UserStat::fetchSolved(fAuthorization::getUserToken()); echo $solved; ?></h3></dt>
-  </dl>
-  <table>
-  <?php
-    $length = 10;
-  	for ($row = 0 ; $row < $solved/$length; $row++ ) {
-  	  echo '<tr>';
-		  for ($problem_id = $row*$length ; $problem_id<($row+1)*$length && $problem_id<$solved; $problem_id++) {
-        echo '<td><h3><a href="./problem/'.$this->solved[$problem_id].'">'.$this->solved[$problem_id].'</a>&nbsp;&nbsp;&nbsp;&nbsp;</h3></td>';
-	    }
-  		echo '</tr>';
-  	}
-  ?>
+    <li><label class="control-lab"><h3>已解决问题：<?php $solved = UserStat::fetchSolved(fAuthorization::getUserToken()); echo $solved; ?></h3></label></li>
+  </ul>
+  <table class="table table-bordered table-striped table-condensed">
+  <?php $length = 10; ?>
+  <?php for ($row = 0 ; $row < $solved/$length; $row++ ): ?>
+  <tr>
+    <?php  for ($problem_id = $row*$length ; $problem_id<($row+1)*$length && $problem_id<$solved; $problem_id++): ?>
+    <td><h3><a href="./problem/<?php echo $this->solved[$problem_id]; ?>"> <?php echo $this->solved[$problem_id]; ?> </a></h3></td>
+    <?php endfor; ?>
+  <tr>
+  <?php endfor; ?>
   </table>
-  <dl>
-    <dt><label class="control-lab"><h3>尝试过的问题：<?php $tried = UserStat::fetchTried(fAuthorization::getUserToken()); echo $tried; ?></h3></dt>
+  <ul>
+    <li><label class="control-lab"><h3>尝试过的问题：<?php $tried = UserStat::fetchTried(fAuthorization::getUserToken()); echo $tried; ?></h3></label></li>
 
-    <dt><label class="control-lab"><h3>尝试但未解决的问题：<?php $failed = $tried-$solved; echo $failed; ?></h3></dt>
-  </dl>
-  <table>
-  <?php
-    $length = 10;
-  	for ($row = 0 ; $row < $failed/$length; $row++ ) {
-  		echo '<tr>';
-		  for ($problem_id = $row*$length ; $problem_id<($row+1)*$length && $problem_id<$failed; $problem_id++) {
-	  			echo '<td><h3><a href="./problem/'.$this->fails[$problem_id].'">'.$this->fails[$problem_id].'</a>&nbsp;&nbsp;&nbsp;&nbsp;</h3></td>';
-	  	}
-  		echo '</tr>';
-  	}
-  ?>
+    <li><label class="control-lab"><h3>尝试但未解决的问题：<?php $failed = $tried-$solved; echo $failed; ?></h3></label></li>
+  </ul>
+  <table class="table table-bordered table-striped table-condensed">
+  <?php for ($row = 0 ; $row < $failed/$length; $row++ ): ?>
+  <tr>
+    <?php  for ($problem_id = $row*$length ; $problem_id<($row+1)*$length && $problem_id<$failed; $problem_id++): ?>
+    <td><h3><a href="./problem/<?php echo $this->fails[$problem_id]; ?>"> <?php echo $this->fails[$problem_id]; ?> </a></h3></td>
+    <?php endfor; ?>
+  <tr>
+  <?php endfor; ?>
   </table>
-  <dl>
-    <dt><label class="control-lab"><h3>已提交问题：<?php echo UserStat::fetchSubmissions(fAuthorization::getUserToken()); ?></h3></dt>
-  </dl>
+  <ul>
+    <li><label class="control-lab"><h3>已提交问题：<?php echo UserStat::fetchSubmissions(fAuthorization::getUserToken()); ?></h3></label></li>
+  </ul>
 <?php
 include(__DIR__ . '/../layout/footer.php');
