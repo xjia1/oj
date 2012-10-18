dc358831c64ea269ca461f8c99250988cdd4d7db
sjtufs <sjtu_fs@outlook.com>
2012-10-18 20:25:33 +0800

    replacement of translate

--- app/translate.php
+++ app/translate.php
@@ -1,373 +1,135 @@
 <?php
-class DashboardController extends ApplicationController
+function translate($string)
 {
-  public function index()
-  {
-    if (!User::can('manage-site')) {
-      fMessaging::create('error', T('You are not allowed to view the dashboard.'));
-      fURL::redirect(Util::getReferer());
-    }
-    
-    if (User::can('view-any-report') or User::can('remove-report')) {
-      $this->reports = fRecordSet::build('report', array(), array('id' => 'desc'));
-    }
-    
-    if (User::can('add-permission') and User::can('remove-permission')) {
-      $this->permissions = fRecordSet::build('Permission');
-    }
-    
-    if (User::can('list-variables')) {
-      $this->variables = fRecordSet::build('Variable');
-    }
-    
-    if (User::can('set-variable')) {
-      if (strlen($edit = fRequest::get('edit'))) {
-        $this->setvar_name = $edit;
-        $this->setvar_value = Variable::getString($edit);
-        $this->setvar_remove = FALSE;
-      } else if (strlen($remove = fRequest::get('remove'))) {
-        $this->setvar_name = $remove;
-        $this->setvar_value = Variable::getString($remove);
-        $this->setvar_remove = TRUE;
-      } else {
-        $this->setvar_name = '';
-        $this->setvar_value = '';
-        $this->setvar_remove = FALSE;
-      }
-    }
-    
-    $this->nav_class = 'dashboard';
-    $this->render('dashboard/index');
-  }
+  static $translations = array(
+    'right now' => '刚刚',
+    'at the same time' => '同时',
+    'second' => '秒',
+    'seconds' => '秒',
+    'minute' => '分钟',
+    'minutes' => '分钟',
+    'hour' => '小时',
+    'hours' => '小时',
+    'day' => '天',
+    'days' => '天',
+    'week' => '周',
+    'weeks' => '周',
+    'month' => '月',
+    'months' => '月',
+    'year' => '年',
+    'years' => '年',
+    '%1$s %2$s' => '%1$s %2$s',
+    '%1$s %2$s from now' => '%1$s %2$s from now',
+    '%1$s %2$s ago' => '%1$s%2$s前',
+    '%1$s %2$s after' => '%1$s %2$s after',
+    '%1$s %2$s before' => '%1$s %2$s before'
+  );
   
-  private function gitPullOriginMaster()
-  { 
-    $data_base_dir = Variable::getString('data-base-path');
-    if (!is_dir($data_base_dir)) {
-      throw new fValidationException(T('Data base directory %s does not exist.'),$data_base_dir);
-    }
-    $pwd = getcwd();
-    chdir($data_base_dir);
-    $output = array();
-    exec('git pull origin master 2>&1', $output, $retval);
-    chdir($pwd);
-    if ($retval != 0) {
-      throw new fValidationException("<pre>{$data_base_dir}$ git pull origin master\n" . implode("\n", $output) . '</pre>');
-    }
+  if (isset($translations[$string])) {
+    return $translations[$string];
   }
-  
-  private function showProblem($id, $ignore_git=FALSE)
-  {
-    try {
-      $problem = new Problem($id);
-      if ($problem->exists()) {
-        throw new fValidationException(T('Problem %s already exists.'),$id);
-      }
-    } catch (fNotFoundException $e) {
-      // fall through
-    }
-    
-    $data_base_dir = Variable::getString('data-base-path');
-    if (!is_dir($data_base_dir)) {
-      throw new fValidationException(T('Data base directory %s does not exist.'),$data_base_dir);
-    }
-    
-    if (!$ignore_git) {
-      $this->gitPullOriginMaster();
-    }
-    
-    $problem_dir = "{$data_base_dir}/problems/{$id}";
-    if (!is_dir($problem_dir)) {
-      throw new fValidationException(T('Problem directory %s does not exist.'),$problem_dir);
-    }
-    
-    $problem_conf = "{$problem_dir}/problem.conf";
-    if (!is_file($problem_conf)) {
-      throw new fValidationException(T('Problem configuration file %s does not exist.'),$problem_conf);
-    }
-    
-    $problem_text = "{$problem_dir}/problem.text";
-    if (!is_file($problem_text)) {
-      throw new fValidationException(T('Problem description file %s does not exist.'),$problem_text);
-    }
-    
-    $data_dir = "{$problem_dir}/data";
-    if (!is_dir($data_dir)) {
-      throw new fValidationException(T('Problem %s does not have a data directory at %s.'),$id,$data_dir);
-    }
-    
-    $properties_content = file_get_contents($problem_conf);
-    $ini_content = str_replace(': ', ' = ', $properties_content);
-    $ini = parse_ini_string($ini_content);
-    if (!array_key_exists('title', $ini) or empty($ini['title'])) {
-      throw new fValidationException(T('Problem title is not specified in problem.conf'));
-    }
-    if (!array_key_exists('author', $ini)) {
-      throw new fValidationException(T('Problem author is not specified in problem.conf'));
-    }
-    if (!array_key_exists('case_count', $ini) or empty($ini['case_count'])) {
-      throw new fValidationException(T('Problem case count is not specified in problem.conf'));
-    }
-    if (!array_key_exists('case_score', $ini) or empty($ini['case_score'])) {
-      throw new fValidationException(T('Problem case score is not specified in problem.conf'));
-    }
-    if (!array_key_exists('time_limit', $ini) or empty($ini['time_limit'])) {
-      throw new fValidationException(T('Problem time limit is not specified in problem.conf'));
-    }
-    if (!array_key_exists('memory_limit', $ini) or empty($ini['memory_limit'])) {
-      throw new fValidationException(T('Problem memory limit is not specified in problem.conf'));
-    }
-    if (!array_key_exists('secret_before', $ini) or empty($ini['secret_before'])) {
-      throw new fValidationException(T('Problem secret-before time is not specified in problem.conf'));
-    }
-    
-    if (empty($ini['author'])) {
-      $ini['author'] = '-';
-    }
-    
-    $problem = new Problem();
-    $problem->setId($id);
-    $problem->setTitle($ini['title']);
-    $problem->setDescription(file_get_contents($problem_text));
-    $problem->setAuthor($ini['author']);
-    $problem->setCaseCount($ini['case_count']);
-    $problem->setCaseScore($ini['case_score']);
-    $problem->setTimeLimit($ini['time_limit']);
-    $problem->setMemoryLimit($ini['memory_limit']);
-    $problem->setSecretBefore($ini['secret_before']);
-    $problem->validate();
-    
-    for ($t = 1; $t <= $problem->getCaseCount(); $t++) {
-      $input = "{$data_dir}/$t.in";
-      if (!is_file($input)) {
-        throw new fValidationException(T('Case input file %s is not found in %s.'),$input,$data_dir);
-      }
-      $output = "{$data_dir}/$t.out";
-      if (!is_file($output)) {
-        throw new fValidationException(T('Case out file %s is not found in %s.'),$output,$data_dir);
-      }
-    }
-    
-    $problem->store();
+  return $string;
+}
+
+function T($english)
+{
+  static $translations = array(
+    'chinese' => array(
+
+	//app/controllers/DashboardController.php
+      'You are not allowed to view the dashboard.' => '你没有进入控制台的权限。',
+	'Problem title is not specified in problem.conf'=>'题目名称在 problem.conf 中没有指出。',
+	'Problem author is not specified in problem.conf'=>'题目作者在 problem.conf 中没有指出。',
+	'Problem case count is not specified in problem.conf'=>'题目数据组数在 problem.conf 中没有指出。',
+	'Problem case score is not specified in problem.conf'=>'题目各组数据分数在 problem.conf 中没有指出。',
+	'Problem time limit is not specified in problem.conf'=>'题目时间限制在 problem.conf 中没有指出。',
+	'Problem memory limit is not specified in problem.conf'=>'题目内存限制在 problem.conf 中没有指出。',
+	'Problem secret-before time is not specified in problem.conf'=>'题目隐藏截止时间在 problem.conf 中没有指出。',
+	'You are not allowed to manage problems.'=>'你没有管理题目的权限。',
+	'All problems refreshed successfully.'=>'所有题目成功刷新。',
+	'You are not allowed to rejudge records.'=>'你没有重判记录的权限。',
+	'Score cannot be negative.'=>'分数不能为负。',
+	'You are not allowed to create reports.'=>'你没有创建比赛的权限。',
+	'Report created successfully.'=>'比赛创建成功。',
+	'You are not allowed to show this report.'=>'你没有显示此比赛的权限。',
+	'You are not allowed to hide this report.'=>'你没有隐藏此比赛的权限。',
+	'You are not allowed to remove this report.'=>'你没有删除此比赛的权限。',
+	'Permission added successfully.'=>'权限添加成功。',
+	'You are not allowed to add permissions.'=>'你没有添加权限的权限。',
+	'Permission removed successfully.'=>'权限删除成功。',
+	'You are not allowed to remove permissions.'=>'你没有删除权限的权限。',
+	'Variable removed successfully.'=>'变量删除成功。',
+	'Variable set successfully.'=>'变量设置成功。',
+	
+	'Data base directory %s does not exist.'=>'数据目录 %s 不存在。',
+	'Problem %s already exists.'=>'题目 %s 已存在。',
+	'Problem directory %s does not exist.'=>'题目目录 %s 不存在。',
+	'Problem configuration file %s does not exist.'=>'题目配置文件 %s 不存在。',
+	'Problem description file %s does not exist.'=>'题目描述文件 %s 不存在。',
+	'Problem %s does not have a data directory at %s.'=>'题目 %s 在 %s 没有数据目录。',
+	'Case input file %s is not found in %s.'=>'数据输入文件 %s 在 %s 没有找到。',
+	'Case output file %s is not found in %s.'=>'数据输出文件 %s 在 %s 没有找到。',
+	'Problem %s showed successfully.'=>'题目 " . func_get_args(1) . " 显示成功。',
+	'Problem %s hidden successfully.'=>'题目 %s 隐藏成功。',
+	'User'=>'用户',
+	'Record'=>'比赛记录',
+
+	//app/controllers/ProblemController.php
+	'Problem is secret now.'=>'题目现在处于隐藏状态。',
+
+	//app/controllers/RecordController.php
+	'You are not allowed to read this record.'=>'你没有查看此记录的权限。',
+
+	//app/controllers/ReportController.php
+	'You are not allowed to view this report.'=>'你没有查看此比赛的权限。',
+	'This contest is not registrable.'=>'比赛不可注册。',
+	'Registered successfully.'=>'注册成功。',
+	'Not allowed to ask question.'=>'不允许提问。',
+	'Please choose a category.'=>'请选择一个分类。',
+	'Question too short (minimum 10 bytes).'=>'问题过短 (最少 10 字节)。',
+	'Question too long (maximum 500 bytes).'=>'问题过长 (最大 500 字节)。',
+	'Question saved.'=>'问题已保存。',
+	'Not allowed to answer question.'=>'没有回答问题的权限。',
+	'Question answered.'=>'问题已回答。',
+	'Not allowed to toggle question visibility.'=>'没有更改此问题可见度的权限。',
+	'Visibility toggled.'=>'可见度已更改。',
+
+	//app/controllers/UserController.php
+	'Logged in successfully.'=>'登录成功。',
+	'Password mismatch.'=>'密码错误。',
+	'Username is too short.'=>'用户名过短。',
+	'Username is too long.'=>'用户名过长。',
+	'Password is too short.'=>'密码过短。',
+	'Username is illegal.'=>'用户名包含非法字符。',
+	'User already exists.'=>'用户已存在。',
+	'Registered successfully.'=>'注册成功。',
+	'Logged out successfully.'=>'注销成功。',
+	'Information updated successfully.'=>'信息更新成功。',
+	'Old password is too short.'=>'旧密码过短。',
+	'New password is too short.'=>'新密码过短。',
+	'Repeat password mismatch.'=>'两次输入密码不相同。',
+	'Old password mismatch.'=>'旧密码错误。',
+	'Password updated successfully.'=>'密码更改成功。',
+	'Invalid email address.'=>'邮箱地址无效。',
+	'Invalid verification code.'=>'验证码无效。',
+	'Your email address is verified successfully.'=>'你的邮箱已成功验证。',
+	'Email verification failed: '=>'邮箱验证失败: ',
+	
+	//app/controllers/SubmitController.php
+	'Code cannot be empty.'=>'代码不能为空。',
+	'Problem is secret now. You are not allowed to submit this problem.'=>'题目现在处于隐藏状态，不能提交此题目。',
+	
+		
+	//app/models/User.php
+	'Click here to verify your email address.'=>'点击此处验证你的邮箱地址。',
+	'You are required to verify your email address before doing this action.'=>'请先验证你的邮箱地址。'
+	
+	
+	)
+  );
+  $language = 'chinese';  // 这里先固定为 chinese，以后会修改成根据用户来决定语言
+  if (isset($translations[$language][$english])) {
+    return $translations[$language][$english];
   }
-  
-  private function hideProblem($id)
-  {
-    $problem = new Problem($id);
-    $problem->delete();
-  }
-  
-  private function refreshProblem($id)
-  {
-    $db = fORMDatabase::retrieve();
-    try {
-      $db->query('BEGIN');
-      $this->hideProblem($id);
-      $this->showProblem($id);
-      $db->query('COMMIT');
-    } catch (fException $e) {
-      $db->query('ROLLBACK');
-      throw $e;
-    }
-  }
-  
-  private function refreshAllProblems()
-  {
-    set_time_limit(0);
-    $db = fORMDatabase::retrieve();
-    try {
-      $db->query('BEGIN');
-      $this->gitPullOriginMaster();
-      $problems = fRecordSet::build('Problem');
-      foreach ($problems as $problem) {
-        $id = $problem->getId();
-        $this->hideProblem($id);
-        $this->showProblem($id, TRUE);  // ignore git
-      }
-      $db->query('COMMIT');
-    } catch (fExpectedException $e) {
-      $db->query('ROLLBACK');
-      throw new fExpectedException($id . ': ' . $e->getMessage());
-    } catch (fUnexpectedException $e) {
-      $db->query('ROLLBACK');
-      throw new fUnexpectedException($id . ': ' . $e->getMessage());
-    }
-  }
-  
-  public function manageProblem($id, $action)
-  {
-    try {
-      if (!User::can('manage-site')) {
-        throw new fAuthorizationException(T('You are not allowed to manage problems.'));
-      }
-      if ($action == 'Show') {
-        $this->showProblem($id);
-        fMessaging::create('success', "Problem {$id} showed successfully.");
-      } else if ($action == 'Hide') {
-        $this->hideProblem($id);
-        fMessaging::create('success', "Problem {$id} hidden successfully.");
-      } else if ($action == 'Refresh') {
-        $this->refreshProblem($id);
-        fMessaging::create('success', "Problem {$id} refreshed successfully.");
-      } else if ($action == 'Refresh All' and User::can('refresh-all')) {
-        $this->refreshAllProblems();
-        fMessaging::create('success', T('All problems refreshed successfully.'));
-      }
-    } catch (fException $e) {
-      fMessaging::create('error', $e->getMessage());
-    }
-    fURL::redirect(Util::getReferer());
-  }
-  
-  public function rejudge($id)
-  {
-    try {
-      if (!User::can('rejudge-record')) {
-        throw new fAuthorizationException(T('You are not allowed to rejudge records.'));
-      }
-      $old_record = new Record($id);
-      $new_record = new Record();
-      $new_record->setOwner($old_record->getOwner());
-      $new_record->setProblemId($old_record->getProblemId());
-      $new_record->setSubmitCode($old_record->getSubmitCode());
-      $new_record->setCodeLanguage($old_record->getCodeLanguage());
-      $new_record->setSubmitDatetime($old_record->getSubmitDatetime());
-      $new_record->setJudgeStatus(JudgeStatus::PENDING);
-      $new_record->setJudgeMessage('Rejudging... PROB=' . $old_record->getProblemId() . ' LANG=' . $old_record->getLanguageName());
-      $new_record->setVerdict(Verdict::UNKNOWN);
-      $new_record->store();
-      fMessaging::create('success', "Record {$id} rejudged.");
-    } catch (fException $e) {
-      fMessaging::create('error', $e->getMessage());
-    }
-    fURL::redirect(Util::getReferer());
-  }
-  
-  public function manjudge($id, $score)
-  {
-    try {
-      if (!User::can('rejudge-record')) {
-        throw new fAuthorizationException(T('You are not allowed to rejudge records.'));
-      }
-      if ($score < 0) {
-        throw new fValidationException(T('Score cannot be negative.'));
-      }
-      $record = new Record($id);
-      $record->manjudge($score);
-      $record->store();
-      fMessaging::create('success', "Record {$id} manually judged.");
-    } catch (fException $e) {
-      fMessaging::create('error', $e->getMessage());
-    }
-    fURL::redirect(Util::getReferer());
-  }
-  
-  public function createReport()
-  {
-    try {
-      if (!User::can('create-report')) {
-        throw new fAuthorizationException(T('You are not allowed to create reports.'));
-      }
-      $report = new Report();
-      $report->setVisible(fRequest::get('visible', 'integer'));
-      $report->setTitle(fRequest::get('title', 'string'));
-      $report->setProblemList(fRequest::get('problem_list', 'string'));
-      $report->setUserList(fRequest::get('user_list', 'string'));
-      $report->setStartDatetime(fRequest::get('start_time', 'timestamp'));
-      $report->setEndDatetime(fRequest::get('end_time', 'timestamp'));
-      $report->store();
-      fMessaging::create('success', T('Report created successfully.'));
-    } catch (fException $e) {
-      fMessaging::create('error', $e->getMessage());
-    }
-    fURL::redirect(Util::getReferer());
-  }
-  
-  public function manageReport($id, $action)
-  {
-    try {
-      $report = new Report($id);
-      if ($action == 'Show') {
-        if (User::can('view-any-report')) {
-          $report->setVisible(1);
-          $report->store();
-          fMessaging::create('success', "Report {$id} showed successfully.");
-        } else {
-          throw new fAuthorizationException(T('You are not allowed to show this report.'));
-        }
-      } else if ($action == 'Hide') {
-        if (User::can('view-any-report')) {
-          $report->setVisible(0);
-          $report->store();
-          fMessaging::create('success', "Report {$id} hidden successfully.");
-        } else {
-          throw new fAuthorizationException(T('You are not allowed to hide this report.'));
-        }
-      } else if ($action == 'Remove') {
-        if (User::can('remove-report')) {
-          $report->delete();
-          fMessaging::create('success', "Report {$id} removed successfully.");
-        } else {
-          throw new fAuthorizationException(T('You are not allowed to remove this report.'));
-        }
-      }
-    } catch (fException $e) {
-      fMessaging::create('error', $e->getMessage());
-    }
-    fURL::redirect(Util::getReferer());
-  }
-  
-  public function managePermission($action)
-  {
-    try {
-      $user_name = fRequest::get('user_name');
-      $permission_name = fRequest::get('permission_name');
-      if ($action == 'Add') {
-        if (User::can('add-permission')) {
-          $permission = new Permission();
-          $permission->setUserName($user_name);
-          $permission->setPermissionName($permission_name);
-          $permission->store();
-          fMessaging::create('success', T('Permission added successfully.'));
-        } else {
-          throw new fAuthorizationException(T('You are not allowed to add permissions.'));
-        }
-      } else if ($action == 'Remove') {
-        if (User::can('remove-permission')) {
-          $permission = new Permission(array('user_name' => $user_name, 'permission_name' => $permission_name));
-          $permission->delete();
-          fMessaging::create('success', T('Permission removed successfully.'));
-        } else {
-          throw new fAuthorizationException(T('You are not allowed to remove permissions.'));
-        }
-      }
-    } catch (fException $e) {
-      fMessaging::create('error', $e->getMessage());
-    }
-    fURL::redirect(Util::getReferer());
-  }
-  
-  public function setVariable()
-  {
-    try {
-      if (fRequest::get('remove', 'boolean')) {
-        $variable = new Variable(fRequest::get('name'));
-        $variable->delete();
-        fMessaging::create('success', T('Variable removed successfully.'));
-      } else {
-        try {
-          $variable = new Variable(fRequest::get('name'));
-        } catch (fNotFoundException $e) {
-          $variable = new Variable();
-          $variable->setName(fRequest::get('name'));
-        }
-        $variable->setValue(fRequest::get('value'));
-        $variable->store();
-        fMessaging::create('success', T('Variable set successfully.'));
-      }
-    } catch (fException $e) {
-      fMessaging::create('error', $e->getMessage());
-    }
-    fURL::redirect(Util::getReferer());
-  }
+  return $english;
 }
