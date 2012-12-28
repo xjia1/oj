f7c072bbad146ec961945b782897d363556b1305
5lipper <slipper.alive@gmail.com>
2012-12-28 15:28:40 +0800

    fix #45

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -38,6 +38,13 @@
 })();
 </script>
 <?php endif; ?>
+<script>
+  $('document').ready(function() {
+    if ($("[rel=tooltip]").length) {
+      $("[rel=tooltip]").tooltip();
+    }
+  });
+</script>
 <?php if (isset($javascripts)): ?>
   <?php foreach ($javascripts as $javascript): ?>
     <script src="<?php echo ASSET_JS; ?>/js/<?php echo $javascript; ?>.js"></script>
