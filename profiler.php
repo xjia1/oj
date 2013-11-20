28ec491e6d37b1d647b3d1e68f993495ddf5fe66
Xiao Jia <stfairy@gmail.com>
2013-11-20 13:57:56 +0800

    add instrumentation util

--- profiler.php
+++ profiler.php
@@ -22,15 +22,45 @@ function profiler_render_begin()
   $profiler_stats['render_time'] = microtime(TRUE);
 }
 
+function profiler_instrument_begin($inst_id)
+{
+  global $profiler_stats;
+  if (array_key_exists($inst_id, $profiler_stats)) {
+    $profiler_stats[$inst_id]['last'] = microtime(TRUE);
+  } else {
+    $profiler_stats[$inst_id] = array(
+      'time' => 0.0,
+      'last' => microtime(TRUE);
+    );
+  }
+}
+
+function profiler_instrument_end($inst_id)
+{
+  global $profiler_stats;
+  if (array_key_exists($inst_id, $profiler_stats)) {
+    $last = $profiler_stats[$inst_id]['last'];
+    $duration = microtime(TRUE) - $last;
+    $profiler_stats[$inst_id]['time'] += $duration;
+  }
+}
+
+function profiler_instrument_read($inst_id)
+{
+  global $profiler_stats;
+  return round($profiler_stats[$inst_id]['time'], 4);
+}
+
 function profiler_dump()
 {
   global $profiler_stats;
   $time_start = $profiler_stats['time_start'];
   $query_num  = $profiler_stats['query_num'];
-  $query_time = round($profiler_stats['query_time'], 6);
-  $time_cost  = round(microtime(TRUE) - $time_start, 6);
-  $render_time = round(microtime(TRUE) - $profiler_stats['render_time'], 6);
-  echo "{$time_cost} sec - render ${render_time} sec - {$query_num} queries - {$query_time} sec @ " . INSTANCE_NAME;
+  $query_time = round($profiler_stats['query_time'], 4);
+  $time_cost  = round(microtime(TRUE) - $time_start, 4);
+  $render_time = round(microtime(TRUE) - $profiler_stats['render_time'], 4);
+  $instruments = profiler_instrument_read('Record::getTimeCost').' '.profiler_instrument_read('Record::getMemoryCost').' '.profiler_instrument_read('Record::getScore');
+  echo "{$time_cost} - render ${render_time} - instru ${instruments} - {$query_num} queries - {$query_time} @ " . INSTANCE_NAME;
   if (SQL_DEBUG) {
     echo "<pre>" . implode("\n", $profiler_stats['statements']) . "</pre>";
   }
