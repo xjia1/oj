ac07b1acf0f4b7f57e8ce73c5c062286ca131607
dataisland <dataisland@outlook.com>
2019-09-27 20:32:03 +0800

    兼容markdown各种格式

--- app/views/problem/show.php
+++ app/views/problem/show.php
@@ -13,6 +13,23 @@ include(__DIR__ . '/../layout/header.php');
     </div>
   </aside>
 </div>
-<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"]  ],
+        processEscapes: true,
+        skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+    }
+});
+
+MathJax.Hub.Queue(function() {
+    var all = MathJax.Hub.getAllJax(), i;
+    for(i=0; i < all.length; i += 1) {
+        all[i].SourceElement().parentNode.className += ' has-jax';
+    }
+});
+</script>
+
+<script async src="//cdn.bootcss.com/mathjax/2.7.0/MathJax.js?config=TeX-MML-AM_CHTML"></script>
 <?php
 include(__DIR__ . '/../layout/footer.php');
